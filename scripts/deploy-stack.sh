dfe322527d07ef4d20f13777169d749588519eb3
Miguel Rodas <tripclocks@gmail.com>
2025-05-13 22:09:00 -0500

    Airflow added to data platform

--- scripts/deploy-stack.sh
+++ scripts/deploy-stack.sh
@@ -9,8 +9,11 @@ echo "✅ MinIO installed."
 echo "🚀 Installing Airbyte..."
 helm install airbyte airbyte/airbyte -n airbyte --create-namespace -f values/airbyte-values.yaml
 
-
 echo "✅ Airbyte installed."
+
+echo "🚀 Installing Airflow..."
+helm install airflow apache-airflow/airflow -n airflow --create-namespace -f values/airflow-values.yaml
+echo "✅ Airflow installed."
 
 echo "🔧 Patching Airbyte webapp service to NodePort..."
 kubectl patch svc airbyte-airbyte-webapp-svc -n airbyte \
@@ -34,5 +37,22 @@ echo "🔧 Patching MinIO console service to NodePort..."
 kubectl patch svc minio-console -n minio \
   -p '{"spec": {"type": "NodePort", "ports": [{"port": 9001, "targetPort": 9001, "protocol": "TCP", "nodePort": 30001}]}}'
 
-echo "🎉 All components deployed successfully!"
+echo "🔧 Patching Airflow API server service to NodePort..."
+kubectl patch svc airflow-api-server -n airflow \
+  --type merge \
+  -p '{
+    "spec": {
+      "type": "NodePort",
+      "ports": [
+        {
+          "name": "http",
+          "port": 8080,
+          "targetPort": 8080,
+          "protocol": "TCP",
+          "nodePort": 30004
+        }
+      ]
+    }
+  }'
 
+echo "🎉 All components (MinIO, Airbyte, Airflow) deployed successfully!"
